37316e2c0c37b9822230f966a606c825e7044814
ttesla <omer.akyol@gmail.com>
2017-04-22 16:21:52 +0300

    second commit

---
+++ Assets/Scripts/FPSman.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSman : MonoBehaviour
+{
+    public Transform Muzzle;
+    public GameObject Bullet;
+    public float Force;
+    public GameObject Flash;
+
+    private float FlashMaxTimer = 0.075f;
+    private float mFlashTimer = 0.0f;
+
+    private bool mShoot = false;
+
+
+	void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mShoot = true;
+            Flash.SetActive(true);
+
+            mFlashTimer = 0.0f;
+        }
+
+        if(mFlashTimer > FlashMaxTimer)
+        {
+            Flash.SetActive(false);
+        }
+
+        mFlashTimer += Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        if (mShoot)
+        {
+            var bulletObj = GameObject.Instantiate(Bullet, Muzzle.position, Quaternion.identity);
+            bulletObj.GetComponent<Rigidbody>().AddForce(Muzzle.forward * Force, ForceMode.Impulse);
+
+            mShoot = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Mission.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("GOREV TAMAM");
+    }
+}
